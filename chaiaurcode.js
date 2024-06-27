ed66b332820d30a267956b2ef3ee930fabd7d283
Chandraprakash Pandey <cp.o.pandey@gmail.com>
2024-06-28 01:55:40 +0530

    Add files via upload

---
+++ chaiaurcode.js
@@ -0,0 +1,9 @@
+const clock = document.querySelector("#clock");
+
+
+
+setInterval(function (){
+    let date = new Date()
+    // console.log(date.toLocaleTimeString());
+    clock.innerHTML = date.toLocaleTimeString()
+}, 1000) // 1000 is 1 sec
